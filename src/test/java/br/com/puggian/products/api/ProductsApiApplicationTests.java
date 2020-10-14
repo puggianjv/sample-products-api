607d5cdcb73854cfbad3283de176713c06a41755
John Puggian <puggianjv@gmail.com>
2020-10-14 09:05:48 -0300

    Adding project structure

---
+++ src/test/java/br/com/puggian/products/api/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.puggian.products.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
