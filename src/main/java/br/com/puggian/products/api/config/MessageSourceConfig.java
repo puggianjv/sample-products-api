e19bb5bc67f4fd16f746124664707c37ffadeba9
John Puggian <puggianjv@gmail.com>
2020-10-19 17:09:30 -0300

    splitting config in multiple classes

--- src/main/java/br/com/puggian/products/api/config/MessageSourceConfig.java
+++ src/main/java/br/com/puggian/products/api/config/MessageSourceConfig.java
@@ -1,34 +1,15 @@
 package br.com.puggian.products.api.config;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class AppConfig {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+public class MessageSourceConfig {
 
     @Bean
     public MessageSource messageSource() {
@@ -44,5 +25,4 @@ public class AppConfig {
         bean.setValidationMessageSource(messageSource());
         return bean;
     }
-
 }
