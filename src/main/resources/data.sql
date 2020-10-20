b7f8c4c81f34053c19512e69323316cfd91cb224
John Puggian <puggianjv@gmail.com>
2020-10-20 03:47:41 -0300

    improving data.sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
-DELETE FROM supplier;
-INSERT INTO supplier(id, name, creation, last_update)
-VALUES (1, 'Dia', '2020-08-08 08:00:00', '2020-08-08 08:00:00'),
-       (2, 'Carrefour', '2020-09-09 09:00:00', '2020-09-09 09:00:00'),
-       (3, 'Extra', '2020-10-10 10:00:00', '2020-10-10 10:00:00');
+INSERT IGNORE INTO supplier(id, name, creation, last_update)
+VALUES (1, 'Dia', now(), now() ),
+       (2, 'Carrefour', now(), now()),
+       (3, 'Extra', now(), now());
