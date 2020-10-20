cef26943f15b9927ac2610ce998a33d8747ca603
John Puggian <puggianjv@gmail.com>
2020-10-20 01:12:25 -0300

    adding docker configuration

--- src/main/java/br/com/puggian/products/api/config/MapperConfig.java
+++ src/main/java/br/com/puggian/products/api/config/MapperConfig.java
@@ -3,10 +3,8 @@ package br.com.puggian.products.api.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class MapperConfig {
 
     @Bean
--- src/main/java/br/com/puggian/products/api/config/MessageSourceConfig.java
+++ src/main/java/br/com/puggian/products/api/config/MessageSourceConfig.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class MessageSourceConfig {
 
     @Bean
