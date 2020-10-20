9c54d61ab02f7bb8dc26bfa75d88d0d9d9bf841b
John Puggian <puggianjv@gmail.com>
2020-10-20 02:58:45 -0300

    updating config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+DELETE FROM supplier;
 INSERT INTO supplier(id, name, creation, last_update)
 VALUES (1, 'Dia', '2020-08-08 08:00:00', '2020-08-08 08:00:00'),
        (2, 'Carrefour', '2020-09-09 09:00:00', '2020-09-09 09:00:00'),
